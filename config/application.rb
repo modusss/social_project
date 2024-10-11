c6d2ce7ab16c492b0be775cbf23e3dda13ab1ad7
Fillype <fillypef@gmail.com>
2024-10-11 09:41:58 -0300

    feat(layout): add shared navigation partial and configure asset precompilation
    Introduced a new shared navigation partial for consistent site navigation, enhancing the user interface by updating application layout to render it. Configured asset compilation by setting `config.assets.compile` to true and adding `application.tailwind.css` to the precompiled assets in the Rails initializer. This ensures that necessary styles are available in production, improving site performance and maintainability.

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module SocialProjectApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.compile = true
   end
 end
