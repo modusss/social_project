5dd1ec81396fc0474bddd256bc0267cbf9cc429a
Fillype <fillypef@gmail.com>
2024-10-12 19:24:46 -0300

    feat(models): add associations for project and region in Visit
    Added `has_one` associations for `visited_project`, `project`, and `region` in the Visit model to support new relationship structures. This change allows more detailed tracking of visits' related projects and geographic regions, improving data linkage and retrieval capabilities. Also updated nested attribute handling to include `visited_project`.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,10 +3,14 @@ class Visit < ApplicationRecord
   belongs_to :user
   has_many :observations, dependent: :destroy
   has_many :pending_needs, dependent: :destroy
+  has_one :visited_project, dependent: :destroy
+  has_one :project, through: :visited_project
+  has_one :region, through: :visited_project
   
   accepts_nested_attributes_for :family
   accepts_nested_attributes_for :observations, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :pending_needs, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :visited_project, reject_if: :all_blank, allow_destroy: true
 
   validates :visit_date, presence: true
   validates :user_id, presence: true
