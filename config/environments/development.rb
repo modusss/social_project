6bb7262b7c16c8bf8c9428af596e291cb397b284
Fillype <fillypef@gmail.com>
2024-10-27 09:27:48 -0300

    add resources to assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  config.assets.debug = true  # Carrega cada asset separadamente, útil para debug em desenvolvimento
+  config.assets.compile = true  # Habilita compilação dinâmica de assets
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
