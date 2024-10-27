2d135e1fb2c4dfbbda9653581e8d0f36b9058006
Fillype <fillypef@gmail.com>
2024-10-27 08:54:36 -0300

    adjust flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def flash_message
+    return unless flash[:notice] || flash[:alert]
+
+    message = flash[:notice] || flash[:alert]
+    content_tag(:p, message, class: "py-2 px-3 bg-green-50 mb-5 text-green-500 font-medium rounded-lg inline-block", id: "flash-message")
+  end
 end
