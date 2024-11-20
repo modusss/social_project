19d044e719cdda8b26a48eedadb6dd64eaafca1e
Fillype <fillypef@gmail.com>
2024-11-20 11:40:28 -0300

    refactor(registration): improve update logic and form rendering
    Simplified the update_resource method to handle email changes and password updates more intuitively. Consolidated duplicate form rendering code in the registration edit view to enhance maintainability and clarity. These changes aim to reduce redundancy and improve user experience during the update process.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,13 +4,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    if params[:password].blank? && params[:password_confirmation].blank?
+    if params[:email] != resource.email || !params[:password].blank?
+      resource.update_with_password(params)
+    else
       params.delete(:password)
       params.delete(:password_confirmation)
       params.delete(:current_password)
       resource.update_without_password(params)
-    else
-      resource.update_with_password(params)
     end
   end
 
