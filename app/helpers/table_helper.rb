1d7fcce6cd2ec4d0d9b2afcf7a7bd090aae48fb5
Fillype <fillypef@gmail.com>
2024-11-20 15:29:10 -0300

    add overflow on table

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -2,19 +2,21 @@ module TableHelper
 
     # Tabela simples com classes CSS atualizadas
     def table(headers, body, table_id)
-      content_tag :table, id: table_id, class: 'highlight  elegant-table' do
-        concat(content_tag(:thead) do
-          content_tag(:tr) do
-            headers.map { |header| content_tag(:th, header, class: 'header-cell center-align') }.join.html_safe
-          end
-        end)
-        concat(content_tag(:tbody) do
-          body.map do |row|
+      content_tag :div, class: 'overflow-x-auto w-full' do
+        content_tag :table, id: table_id, class: 'highlight elegant-table w-full' do
+          concat(content_tag(:thead) do
             content_tag(:tr) do
-              row.map { |column| content_tag(:td, column, class: 'data-cell center-align') }.join.html_safe
+              headers.map { |header| content_tag(:th, header, class: 'header-cell center-align') }.join.html_safe
             end
-          end.join.html_safe
-        end)
+          end)
+          concat(content_tag(:tbody) do
+            body.map do |row|
+              content_tag(:tr) do
+                row.map { |column| content_tag(:td, column, class: 'data-cell center-align') }.join.html_safe
+              end
+            end.join.html_safe
+          end)
+        end
       end
     end
   
@@ -63,19 +65,21 @@ module TableHelper
   
     # Tabela usando classes Tailwind CSS
     def tailwind_table(headers, body, table_id)
-      content_tag :table, id: table_id, class: 'min-w-full divide-y divide-gray-200' do
-        concat(content_tag(:thead, class: 'bg-gray-50') do
-          content_tag(:tr) do
-            headers.map { |header| content_tag(:th, header, class: 'px-6 py-3 text-center text-gray-700 uppercase tracking-wider font-bold text-lg') }.join.html_safe
-          end
-        end)
-        concat(content_tag(:tbody, class: 'bg-white divide-y divide-gray-200') do
-          body.map do |row|
-            content_tag(:tr, class: 'hover:bg-gray-50') do
-              row.map { |column| content_tag(:td, column, class: 'px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center') }.join.html_safe
+      content_tag :div, class: 'overflow-x-auto w-full' do
+        content_tag :table, id: table_id, class: 'min-w-full divide-y divide-gray-200' do
+          concat(content_tag(:thead, class: 'bg-gray-50') do
+            content_tag(:tr) do
+              headers.map { |header| content_tag(:th, header, class: 'px-6 py-3 text-center text-gray-700 uppercase tracking-wider font-bold text-lg') }.join.html_safe
             end
-          end.join.html_safe
-        end)
+          end)
+          concat(content_tag(:tbody, class: 'bg-white divide-y divide-gray-200') do
+            body.map do |row|
+              content_tag(:tr, class: 'hover:bg-gray-50') do
+                row.map { |column| content_tag(:td, column, class: 'px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center') }.join.html_safe
+              end
+            end.join.html_safe
+          end)
+        end
       end
     end
   
