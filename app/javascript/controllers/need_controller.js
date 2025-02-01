6b5dc98bb32813eb1e4969e63f41b15404750b89
Fillype <fillypef@gmail.com>
2025-02-01 18:57:50 -0300

    feat(needs): enhance beneficiary selection with clickable rows and Stimulus

--- app/javascript/controllers/need_controller.js
+++ app/javascript/controllers/need_controller.js
@@ -1,6 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  toggleCheckbox(event) {
+    // Não dispara se o clique foi diretamente no checkbox
+    if (event.target.type !== 'checkbox') {
+      const checkbox = event.currentTarget.querySelector('input[type="checkbox"]')
+      checkbox.checked = !checkbox.checked
+      checkbox.dispatchEvent(new Event('change'))
+    }
+  }
+
   updateBeneficiaries(event) {
     const checkboxes = event.target.closest('.my-5').querySelectorAll('input[type="checkbox"]')
     const hiddenField = event.target.closest('.my-5').querySelector('input[type="hidden"]')
