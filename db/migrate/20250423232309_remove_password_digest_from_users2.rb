6fd5ae6ab2edcee534a37e4c6ecf5353b4086243
Fillype <fillypef@gmail.com>
2025-04-23 20:24:14 -0300

    refactor(users): remove password_digest column from users table
    - Removed the `password_digest` column from the `users` table to enhance security and simplify user data management.
    - Updated the schema version to reflect this change in the database structure.

---
+++ db/migrate/20250423232309_remove_password_digest_from_users2.rb
@@ -0,0 +1,5 @@
+class RemovePasswordDigestFromUsers2 < ActiveRecord::Migration[7.2]
+  def change
+    remove_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_23_231007) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_23_232309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -119,7 +119,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_23_231007) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
