56f26980cfade077904ec4b8386f118b6e8eae5f
Fillype <fillypef@gmail.com>
2025-04-23 20:25:42 -0300

    refactor(users): remove password_digest from user creation parameters in tests
    - Updated the user creation test to exclude the `password_digest` parameter, aligning with recent changes in the users table structure for enhanced security and data management.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: @user.email, name: @user.name, password_digest: @user.password_digest, phone: @user.phone, role: @user.role } }
+      post users_url, params: { user: { email: @user.email, name: @user.name, phone: @user.phone, role: @user.role } }
     end
 
     assert_redirected_to user_url(User.last)
